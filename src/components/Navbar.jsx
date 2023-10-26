e829decc2aede5983df342de75d8b8d655ee8de2
LangPiñeiro <alangp@miumg.edu.gt>
2023-10-26 16:58:37 -0600

    cambios para que funcione navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,20 @@
 "use client";
 import { useSession } from "next-auth/react";
-import NavbarDefault from './NavbarDefault'
+import NavbarDefault from "./NavbarDefault";
 const Navegador = () => {
   const { status } = useSession();
   return (() => {
     switch (status) {
       case "authenticated": //si el usuario esta autenticado retorna el dashboard
         return (
-          <NavbarDefault status={status} />
+           <NavbarDefault status={status}/>
         );
       case "loading":
-        return (
-          <NavbarDefault status={status} />
-        );
+        return <NavbarDefault status={status} />;
       case "unauthenticated":
-        return (
-          <NavbarDefault status={status} />
-        );
+        return <NavbarDefault status={status} />;
       default:
-        return (
-          <NavbarDefault status={status} />
-        );
+        return <NavbarDefault status={status} />;
     }
   })();
 };
